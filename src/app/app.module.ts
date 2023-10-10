741bfec5525d4be09d0add5363d12404441f8011
Heet Doshi <doshiheet2872003@gmail.com>
2023-10-10 11:27:43 +0530

    Added

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponentComponent } from './form-component/form-component.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IntoComponent } from './into/into.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponentComponent,
+    NavbarComponent,
+    IntoComponent,
+    CoursesComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
